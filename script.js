d2e237d33ce6899a21d4306b8a46dce1506b72b8
kaustubh wade <kaustubhwade97@gmail.com>
2020-11-06 23:07:03 +0530

    Mobile SIte Done with minor bugs.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+// function to toggle menu items
 function toggleMobileMenu() {
     var height = document.getElementById("nav-items");
     var toggleIcon = document.getElementById("toggleIcon");
     if (height.style.height == "332px") {
+        // height toggled
         height.style.height = "0px";
-        console.log(toggleIcon.classList);
+
+        // icon toggled
         toggleIcon.classList.remove("fa-times");
         toggleIcon.classList.add("fa-bars");
     } else {
+        // height toggled
         height.style.height = "332px";
+
+        // icon toggled
         toggleIcon.classList.remove("fa-bars");
         toggleIcon.classList.add("fa-times");
     }
